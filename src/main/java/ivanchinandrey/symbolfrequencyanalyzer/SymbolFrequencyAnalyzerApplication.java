e7b1d54ba464877d8f2a7c44046acb5f0ad058b7
AndreyIvanchin <andreyivanchin000@gmail.com>
2024-01-20 10:20:09 +0300

    Initial commit

---
+++ src/main/java/ivanchinandrey/symbolfrequencyanalyzer/SymbolFrequencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ivanchinandrey.symbolfrequencyanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbolFrequencyAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymbolFrequencyAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ivanchinandrey/symbolfrequencyanalyzer/SymbolFrequencyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ivanchinandrey.symbolfrequencyanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbolFrequencyAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
